856d6e1171d1852819529de1bacafdf726efdf88
equdevel <equdevel@gmail.com>
2023-11-27 01:45:08 +0800

    Add pattern search in memory

--- time_control.c
+++ time_control.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <Windows.h>
+#include <windows.h>
 #include <tlhelp32.h>
 #include <stdint.h>
 
@@ -65,9 +65,29 @@ void print_memory(HANDLE hProcess, DWORD_PTR pBuffer, SIZE_T len) {
     SIZE_T bytes_read = 0;
     char local_buffer[len];
     ReadProcessMemory(hProcess, (void*)pBuffer, &local_buffer, len, &bytes_read);
-    printf("\nBuffer: ");
     print_hex(local_buffer, len);
-    printf("\nRead bytes = %d\n\n", bytes_read);
+    printf("\nBytes read = %d\n\n", bytes_read);
+}
+
+DWORD_PTR search_bytes(HANDLE hProcess, DWORD_PTR StartAddress, char *bytes, SIZE_T size) {
+    int READ_SIZE = 1024;
+    char local_buffer[READ_SIZE];
+    DWORD_PTR Address = StartAddress;
+    while(ReadProcessMemory(hProcess, (void*)Address, &local_buffer, READ_SIZE, 0)) {
+        BOOL found = TRUE;
+        for(int i=0; i<READ_SIZE; i++) {
+            found = TRUE;
+            for(int j=0; j<size; j++) {
+                if(bytes[j]!=local_buffer[i+j]) {
+                    found = FALSE;
+                    break;
+                }
+            }
+            if(found) return Address + i;
+        }
+        Address += READ_SIZE;
+    }
+    return 0;
 }
 
 void main()
@@ -78,36 +98,45 @@ void main()
     char *PrName = "SnowRunner.exe";
     if (!(PID = get_PID(PrName)))
     {
-        printf("Process does not exist\n");
+        printf("Process %s not found!\n\n", PrName);
         system("pause");
         return;
     }
-    printf("Process founded!\n");
-    printf("Process name: %s\n", PrName);
-    printf("PID: %d\n\n", PID);
+    printf("Process %s found!\n", PrName);
+    printf("PID = %d\n\n", PID);
 
     if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID)))
     {
-        printf("OpenProcess error\n");
+        printf("OpenProcess error\n\n");
+        system("pause");
         return;
     }
 
     printf("OpenProcess is ok\n");
     printf("Now we have handle of process %s\n", PrName);
-    printf("Handle: %d\n\n", hProcess);
+    printf("Handle = %d\n\n", hProcess);
 
 
     if (!(BaseAddress = GetModuleBase(PrName, PID)))
     {
-        printf("GetModuleBase error\n");
+        printf("GetModuleBase error\n\n");
+        system("pause");
         return;
     }
     printf("GetModuleBase is ok\n");
     printf("BaseAddress: %llx\n\n", BaseAddress);
 
-    DWORD_PTR pBuffer = BaseAddress + 0xA8C306;
-    printf("pBuffer: %llx\n", pBuffer);
+    //DWORD_PTR pBuffer = BaseAddress + 0xA8C306;
+    DWORD_PTR pBuffer = search_bytes(hProcess, BaseAddress, "\xF3\x41\x0F\x11\x95\x38\x01\x00\x00\xF3", 10);
+    if(pBuffer != 0)
+        printf("Found pattern address = %llx\n", pBuffer);
+    else {
+        printf("Pattern not found in memory!\n\n");
+        system("pause");
+        return;
+    }
 
+    printf("Memory before injection = ");
     print_memory(hProcess, pBuffer, 9);
 
     //INJECTION
@@ -120,11 +149,17 @@ void main()
     //BOOL result = WriteProcessMemory(hProcess, (LPVOID)pInjectedBuffer, new_buffer, 12, &bytes_written);
     BOOL result = PatchEx(hProcess, (LPVOID)pBuffer, new_buffer, 9, &bytes_written);
     //hProcThread = CreateRemoteThread(hProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)pInjectedFunction, NULL, NULL, NULL);
+    DWORD last_error = GetLastError();
+    printf("CODE INJECTION:\n");
     printf("Result code = %d\n", result);
-    printf("Written bytes = %d\n", bytes_written);
-    printf("Last error = %d\n", GetLastError());
+    printf("Bytes written = %d\n", bytes_written);
+    printf("Last error = %d\n\n", last_error);
 
+    printf("Memory after injection = ");
     print_memory(hProcess, pBuffer, 9);
+
+    if(result==1 && bytes_written==9 && last_error==0)
+        printf("SnowRunner timer was stopped!\n\n");
 
     system("pause");
 }
